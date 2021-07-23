096c7b4868e9ae39d1a1b7ab2554d587b7c60a0f
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2021-07-23 16:31:50 +0200

    Revert "drm: msm: implement fod handling"
    This reverts commit 79e37c8b1099ae81c476a4e89a9d41c18e4aa335.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -172,12 +172,6 @@ struct drm_panel_esd_config {
 	u32 groups;
 };
 
-#define BRIGHTNESS_ALPHA_PAIR_LEN 2
-struct brightness_alpha_pair {
-	u32 brightness;
-	u32 alpha;
-};
-
 struct dsi_panel {
 	const char *name;
 	const char *type;
@@ -230,9 +224,6 @@ struct dsi_panel {
 	enum dsi_panel_physical_type panel_type;
 
 	bool doze_enabled;
-
-	struct brightness_alpha_pair *fod_dim_lut;
-	u32 fod_dim_lut_count;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -354,9 +345,5 @@ int dsi_panel_parse_esd_reg_read_configs(struct dsi_panel *panel);
 void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
 
 int dsi_panel_set_doze_status(struct dsi_panel *panel, bool status);
-
-int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
-
-u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
