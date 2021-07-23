aa451d0cfb62ee0f215cf13569f9adf40b07bd1f
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2021-07-23 16:31:40 +0200

    Revert "drm/msm/dsi-staging: fix power check in idle hints"
    This reverts commit 95bd3d382ca3f6c42fba385c15d7dc2521746790.

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -67,12 +67,6 @@ static const struct of_device_id dsi_display_dt_match[] = {
 
 struct dsi_display *primary_display;
 
-static inline bool is_lp_mode(int power_mode)
-{
-	return power_mode == SDE_MODE_DPMS_LP1 ||
-			power_mode == SDE_MODE_DPMS_LP2;
-}
-
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
 {
@@ -1095,7 +1089,9 @@ int dsi_display_set_power(struct drm_connector *connector,
 		msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK, &notify_data);
 		break;
 	case SDE_MODE_DPMS_ON:
-		if (is_lp_mode(display->panel->power_mode)) {
+		if (display->panel->power_mode == SDE_MODE_DPMS_LP1 ||
+			display->panel->power_mode == SDE_MODE_DPMS_LP2) {
+			msm_drm_notifier_call_chain(MSM_DRM_EARLY_EVENT_BLANK, &notify_data);
 			rc = dsi_panel_set_nolp(display->panel);
 			msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK, &notify_data);
 		}
@@ -6655,8 +6651,7 @@ void dsi_display_set_idle_hint(void *dsi_display, bool is_idle)
 	if (unlikely(!display || !display->panel))
 		return;
 
-	if (!dsi_panel_initialized(display->panel) ||
-	    is_lp_mode(display->panel->power_mode))
+	if (display->panel->power_mode != DRM_MODE_DPMS_ON)
 		return;
 
 	if (is_idle)
